6559cd2f3fb9fb63695ba7c6a9bf238e3596581b
menabebawy1 <mena.m.bebawy@gmail.com>
2020-11-07 00:46:26 -0500

    remove duplicate

---
+++ practice.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int removeDuplicates(vector<int> &nums){
+        if (nums.size() > 0){
+            for (int i = 0; i < nums.size() - 1; i++){
+                if (nums[i] == nums[i + 1]){
+                    nums.erase(nums.begin() + i);
+                    i--;
+                }
+            }
+        }
+        return nums.size();
+    };
+
+int main()
+{
+    vector<int> numbers;
+    numbers.push_back(1);
+    numbers.push_back(1);
+    numbers.push_back(2);
+    numbers.push_back(2);
+    numbers.push_back(2);
+    numbers.push_back(3);
+    numbers.push_back(3);
+    numbers.push_back(4);
+    numbers.push_back(4);
+   
+
+
+
+    removeDuplicates(numbers);
+
+
+    
+
+
+    for(int i = 0; i < numbers.size(); i++){
+        cout << numbers[i] << endl;
+    }
+
+    // removeDuplicates(numbers);
+}
