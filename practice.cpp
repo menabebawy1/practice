d7c3077cda99c570f694a9e4810fb8216f13a162
menabebawy1 <mena.m.bebawy@gmail.com>
2020-11-07 00:50:25 -0500

    added solution 2

--- practice.cpp
+++ practice.cpp
@@ -2,17 +2,31 @@
 #include <vector>
 using namespace std;
 
-int removeDuplicates(vector<int> &nums){
-        if (nums.size() > 0){
-            for (int i = 0; i < nums.size() - 1; i++){
-                if (nums[i] == nums[i + 1]){
-                    nums.erase(nums.begin() + i);
-                    i--;
-                }
+int removeDuplicates(vector<int> &nums)
+{
+    if (nums.size() > 0)
+    {
+        for (int i = 0; i < nums.size() - 1; i++)
+        {
+            if (nums[i] == nums[i + 1])
+            {
+                nums.erase(nums.begin() + i);
+                i--;
             }
         }
-        return nums.size();
-    };
+    }
+    return nums.size();
+};
+
+int removeDuplicates(vector<int> &nums)//Solution
+{
+    nums.erase(unique(nums.begin(), nums.end()), nums.end()); //This deletes everything after unique
+
+    //erase(from, to) erases this range
+    //unique(from, to) //gets the unique values in this range
+
+    return nums.size();
+}
 
 int main()
 {
@@ -26,17 +40,11 @@ int main()
     numbers.push_back(3);
     numbers.push_back(4);
     numbers.push_back(4);
-   
-
-
 
     removeDuplicates(numbers);
 
-
-    
-
-
-    for(int i = 0; i < numbers.size(); i++){
+    for (int i = 0; i < numbers.size(); i++)
+    {
         cout << numbers[i] << endl;
     }
 
